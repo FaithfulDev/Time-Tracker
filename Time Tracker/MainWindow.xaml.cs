b2ba53ef4aec0781208521b4850eaa4628a311fe
Marcel Rütjerodt <marcelr1989@outlook.com>
2019-01-06 22:43:57 +0100

    Added Tray Icon + Logic

--- Time Tracker/MainWindow.xaml.cs
+++ Time Tracker/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace Time_Tracker
         public MainWindow()
         {
             InitializeComponent();
+            SetStartupPosition();
+        }
+
+        private void SetStartupPosition()
+        {
+            double iScreenHeight = SystemParameters.WorkArea.Height;
+            double iScreenWidth = SystemParameters.WorkArea.Width;
+
+            this.Left = iScreenWidth - this.Width - 10;
+            this.Top = iScreenHeight - this.Height - 10;
+        }
+
+        private void UiTrayIcon_TrayMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+            this.Activate();
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void MenuItem_Quit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
         }
     }
 }
